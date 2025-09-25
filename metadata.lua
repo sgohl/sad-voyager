d55cfdcdbb844fc33f675d87c3a00fb9f09930a4
sgohl <silvio@gohl.it>
2025-09-25 10:19:06 +0200

    Doctor

--- metadata.lua
+++ metadata.lua
@@ -5,11 +5,11 @@ return PlaceObj('ModDef', {
 	'last_changes', "fix skills",
 	'id', "Pmb5Nio",
 	'author', "oivlisLhog",
-	'version', 106,
+	'version', 107,
 	'lua_revision', 233360,
 	'saved_with_revision', 373414,
 	'has_data', true,
-	'saved', 1758788322,
+	'saved', 1758788343,
 	'steam_id', "3573518914",
 	'TagCharacters', true,
 })

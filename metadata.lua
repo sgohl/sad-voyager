4d7730fdc0500b158c37357b858ec3d0f387ce1a
sgohl <silvio@gohl.it>
2025-09-25 10:35:49 +0200

    Seven

--- metadata.lua
+++ metadata.lua
@@ -5,11 +5,11 @@ return PlaceObj('ModDef', {
 	'last_changes', "fix skills",
 	'id', "Pmb5Nio",
 	'author', "oivlisLhog",
-	'version', 109,
+	'version', 110,
 	'lua_revision', 233360,
 	'saved_with_revision', 373414,
 	'has_data', true,
-	'saved', 1758789325,
+	'saved', 1758789346,
 	'steam_id', "3573518914",
 	'TagCharacters', true,
 })

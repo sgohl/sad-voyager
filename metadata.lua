29581ea3a6ec13a3aa8e03b77586dfc6b7d19ab3
sgohl <silvio@gohl.it>
2025-09-25 13:10:52 +0200

    Kes

--- metadata.lua
+++ metadata.lua
@@ -5,11 +5,11 @@ return PlaceObj('ModDef', {
 	'last_changes', "fix skills",
 	'id', "Pmb5Nio",
 	'author', "oivlisLhog",
-	'version', 130,
+	'version', 134,
 	'lua_revision', 233360,
 	'saved_with_revision', 373414,
 	'has_data', true,
-	'saved', 1758796959,
+	'saved', 1758798638,
 	'steam_id', "3573518914",
 	'TagCharacters', true,
 })

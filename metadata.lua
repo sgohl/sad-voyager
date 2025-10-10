4196167893e58cb7bdd7952be091412599f05bf9
sgohl <silvio@gohl.it>
2025-10-10 18:18:38 +0200

    152

--- metadata.lua
+++ metadata.lua
@@ -5,11 +5,11 @@ return PlaceObj('ModDef', {
 	'last_changes', "- Tuvok: add Vegetarian\n- Seven: remove Hemophobia\n- Neelix: add Combat(interested)",
 	'id', "Pmb5Nio",
 	'author', "oivlisLhog",
-	'version', 151,
+	'version', 152,
 	'lua_revision', 233360,
 	'saved_with_revision', 373414,
 	'has_data', true,
-	'saved', 1759741474,
+	'saved', 1759742234,
 	'steam_id', "3573518914",
 	'TagCharacters', true,
 })
